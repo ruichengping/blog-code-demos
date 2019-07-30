45074c89d09b647a9673eabc5fe6e90f666f4e7d
长卿 <changqing@dtstack.com>
2019-07-30 12:03:21 +0800

    加入npm-run-all

--- react-ssr/src/index.js
+++ react-ssr/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {hydrate} from 'react-dom';
-import StyleContext from 'isomorphic-style-loader/StyleContext'
+import StyleContext from 'isomorphic-style-loader/StyleContext';
 import App from './app';
 const insertCss = (...styles) => {
   const removeCss = styles.map(style => style._insertCss())
